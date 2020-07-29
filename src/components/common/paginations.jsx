56438418152134fbc46ab6f3e6e5ee8a6747b772
zakariabijoy <zakaria@vonome.com>
2020-07-30 01:30:55 +0600

    pagination -displaying pages

--- src/components/common/paginations.jsx
+++ src/components/common/paginations.jsx
@@ -1,7 +1,25 @@
 import React, { Component } from "react";
+import _ from "lodash";
 
-const Pagination = () => {
-  return null;
+const Pagination = (props) => {
+  const { itemsCount, pageSize } = props;
+
+  const pagesCount = Math.ceil(itemsCount / pageSize);
+  const pages = _.range(1, pagesCount + 1);
+
+  if (pagesCount === 1) return null;
+  // [1 ... pagesCount].map()
+  return (
+    <nav>
+      <ul className="pagination">
+        {pages.map((page) => (
+          <li key={page} className="page-item">
+            <a className="page-link">{page}</a>
+          </li>
+        ))}
+      </ul>
+    </nav>
+  );
 };
 
 export default Pagination;
