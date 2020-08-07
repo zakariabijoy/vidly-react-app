8fe8e39460f53e2d7ae1c6b384bcf030b4d0fa53
zakariabijoy <zakaria@vonome.com>
2020-08-08 00:55:43 +0600

    sorting - moving responsibility

--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -44,16 +44,7 @@ class Movies extends Component {
     this.setState({ selectedGenre: genre, currentPage: 1 });
   };
 
-  handleSort = (path) => {
-    const sortColumn = { ...this.state.sortColumn };
-
-    if (sortColumn.path === path)
-      sortColumn.order = sortColumn.order === "asc" ? "desc" : "asc";
-    else {
-      sortColumn.path = path;
-      sortColumn.order = "asc";
-    }
-
+  handleSort = (sortColumn) => {
     this.setState({ sortColumn });
   };
 
@@ -92,6 +83,7 @@ class Movies extends Component {
           <p>Showing {filtered.length} movies in the database</p>
           <MoviesTable
             movies={movies}
+            sortColumn={sortColumn}
             onLike={this.handleLike}
             onDelete={this.handleDelete}
             onSort={this.handleSort}
