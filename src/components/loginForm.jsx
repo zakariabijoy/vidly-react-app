4605f71e1272075e1ef1d85a5573ce7a655f59df
zakariabijoy <zakaria@vonome.com>
2020-08-13 21:37:20 +0600

    handling form submission

--- src/components/loginForm.jsx
+++ src/components/loginForm.jsx
@@ -1,11 +1,19 @@
 import React, { Component } from "react";
 
 class LoginForm extends Component {
+  handleSubmit = (e) => {
+    e.preventDefault();
+
+    // call the server
+
+    console.log("Submitted");
+  };
+
   render() {
     return (
       <div>
         <h1>Login form</h1>
-        <form action="">
+        <form action="" onSubmit={this.handleSubmit}>
           <div className="form-group">
             <label htmlFor="username">Username</label>
             <input type="text" className="form-control" id="username" />
