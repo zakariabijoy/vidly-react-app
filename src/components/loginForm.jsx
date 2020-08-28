1404dea52c14821cef23980764ad6d36b9fd1b63
zakariabijoy <zakaria@vonome.com>
2020-08-29 03:55:46 +0600

    submitting the login form to backend

--- src/components/loginForm.jsx
+++ src/components/loginForm.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Joi from "joi-browser";
 import Form from "./common/form";
+import { login } from "../services/authService";
 
 class LoginForm extends Form {
   state = {
@@ -13,10 +14,10 @@ class LoginForm extends Form {
     password: Joi.string().required().label("Password"),
   };
 
-  doSubmit = () => {
+  doSubmit = async () => {
     // call the server
-
-    console.log("Submitted");
+    const { data } = this.state;
+    await login(data.username, data.password);
   };
 
   render() {
