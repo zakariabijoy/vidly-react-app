9906859f54f5e3bfb247cc52953c9f9fee184b4c
zakariabijoy <zakaria@vonome.com>
2020-09-01 03:39:57 +0600

    Extracting ProtectedRoute

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ import MovieForm from './components/movieForm';
 import LoginForm from './components/loginForm';
 import RegisterForm from './components/registerForm';
 import Logout from './components/logout';
+import ProtectedRoute from "./components/common/protectedRoute";
 import authService from "./services/authService";
 import "./App.css";
 import 'react-toastify/dist/ReactToastify.css';
@@ -33,15 +34,7 @@ function App() {
           <Route path="/register" component={RegisterForm}></Route>
           <Route path="/login" component={LoginForm}></Route>
           <Route path="/logout" component={Logout}></Route>
-          <Route
-            path="/movies/:id"
-            render={props => {
-              console.log(user);
-              if (!user) return <Redirect to="/login" />;
-              return <MovieForm {...props} user={user} />
-            }}
-          >
-          </Route>
+          <ProtectedRoute path="/movies/:id" component={MovieForm} />
           <Route path="/movies" render={props => <Movies {...props} user={user} />}></Route>
           <Route path="/customers" component={Cutomers}></Route>
           <Route path="/rentals" component={Rentals}></Route>
