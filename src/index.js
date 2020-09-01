198a78926d81515b2e59f9d505d89ba1bbfaf8f1
zakariabijoy <zakaria@vonome.com>
2020-09-01 19:38:25 +0600

    Environment variable

--- src/index.js
+++ src/index.js
@@ -7,6 +7,8 @@ import 'bootstrap/dist/css/bootstrap.css';
 import 'font-awesome/css/font-awesome.css';
 import { BrowserRouter } from 'react-router-dom';
 
+console.log(process.env.REACT_APP_NAME);
+
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter><App /></BrowserRouter>
